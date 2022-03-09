a0f5c23f521c034e08b56620bdd4e20c378a836a
Kyungyun Lee, IOS dev <leeky16498@gmail.com>
2022-03-09 20:15:50 +0000

    Initial Commit

---
+++ PapagoApiSelf/PapagoApiSelfApp.swift
@@ -0,0 +1,17 @@
+//
+//  PapagoApiSelfApp.swift
+//  PapagoApiSelf
+//
+//  Created by Kyungyun Lee on 09/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct PapagoApiSelfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
